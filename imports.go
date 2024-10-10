386558aaf3f08bf3a45b1f925fc425e585e3a739
Alex Hall <864349+compuguy@users.noreply.github.com>
2024-10-10 18:46:07 -0400

    Get working with latest caddy

--- imports.go
+++ imports.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package poc-caddy-mqtt-proxy
+package poccaddymqttproxy
 
 import (
 	_ "github.com/caddyserver/caddy/v2/modules/standard"
